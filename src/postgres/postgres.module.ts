d471913c4ae7089ce35e8bf46e03fe95349cd048
Denys Sttrilchuk <denia876@gmail.com>
2024-05-24 15:04:48 +0300

    lesson 4 structure, config, compose

---
+++ src/postgres/postgres.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Config, DatabaseConfig } from '../configs/configs.type';
+import { AuthEntity } from '../database/entities/auth.entity';
+import { UserEntity } from '../database/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<Config>) => {
+        const databaseConfig = configService.get<DatabaseConfig>('database');
+        return {
+          type: 'postgres',
+          host: databaseConfig.host,
+          port: databaseConfig.port,
+          username: databaseConfig.user,
+          password: databaseConfig.password,
+          database: databaseConfig.dbName,
+          entities: [AuthEntity, UserEntity],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class PostgresModule {}
